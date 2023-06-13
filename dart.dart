be245c69680bc9e3cc2171049a3c7279b7ad970c
omaffezzolli <joao.l09@aluno.ifsc.edu.br>
2023-06-13 19:24:39 -0300

    Topico 2 e 3

--- dart.dart
+++ dart.dart
@@ -25,4 +25,49 @@ void main() {
   sobrenome = "Wilian";
   print(sobrenome);
   
+  //3-If e Switch
+  
+ bool seguirEmfrente = false;
+  
+  if(seguirEmfrente){
+    print("Andar");  
+  }
+  else{
+    print("Parar");
+  }
+  
+  if( 10 > 5){
+    print("Dez é maior que cinco");
+  }
+  
+  int valorInt = 0;
+  switch(valorInt){
+    case 0:
+      print("Zero");
+      break;
+    case 1:
+      print ("Um");
+      break;
+    case 2:
+      print ("Dois");
+      break;
+      
+      //Default: se não conter o valor na variavel e nos cases o codigo vai cair no default
+    default:
+      print("Padrão");
+  }
+  
+  //4-Estruturas de repetição
+  
+  for(int i = 0; 0<=10; i++){
+    print(i*2);
+  }
+  //While so para quando a condição acontecer;
+  int contador = 10;
+  while(contador != 1){
+    contador = contador -1;
+    print('loop -> $contador');
+  }
+
+  //5-
 }
